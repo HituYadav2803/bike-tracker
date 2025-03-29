5e2662284b3ec5e3352c3db5fe11915e21755485
hitesh Yadav <124537244+HituYadav2803@users.noreply.github.com>
2025-03-29 23:00:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,25 +1,23 @@
-const CACHE_NAME = 'bike-tracker-v1';
-const urlsToCache = [
+const CACHE_NAME = 'bike-tracker-v4';
+const URLS_TO_CACHE = [
   '/',
   '/index.html',
-  '/styles.css',
-  '/firebase.js',
-  '/map.js',
+  '/app.js',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.css',
   'https://unpkg.com/leaflet@1.9.4/dist/leaflet.js',
   'https://cdn-icons-png.flaticon.com/512/2972/2972185.png'
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
+      .then((cache) => cache.addAll(URLS_TO_CACHE))
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => response || fetch(event.request))
+      .then((response) => response || fetch(event.request))
   );
 });
